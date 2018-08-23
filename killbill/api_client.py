7729961f39999c98328a899d34dbd18b98a12ef6
jungipark <park@jungi.me>
2018-08-23 11:19:25 +0900

    (chore) return http data only default None -> False

--- killbill/api_client.py
+++ killbill/api_client.py
@@ -282,7 +282,7 @@ class ApiClient(object):
     def call_api(self, resource_path, method,
                  path_params=None, query_params=None, header_params=None,
                  body=None, post_params=None, files=None,
-                 response_type=None, auth_settings=None, async=None,
+                 response_type=False, auth_settings=None, async=None,
                  _return_http_data_only=None, collection_formats=None,
                  _preload_content=True, _request_timeout=None):
         """Makes the HTTP request (synchronous) and returns deserialized data.
